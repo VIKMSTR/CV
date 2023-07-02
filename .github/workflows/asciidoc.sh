9f54180f32767e4e96094e5216f2518f8176b025
Viktor Krejčíř <krejcirviktor@gmail.com>
2023-07-02 13:17:24 +0200

    asciidoc

--- .github/workflows/asciidoc.sh
+++ .github/workflows/asciidoc.sh
@@ -9,4 +9,4 @@ ASCIIDOCTOR_PDF_DIR=`gem contents asciidoctor-pdf --show-install-dir`
 
 asciidoctor -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/html/ -o index.html CV.adoc
 #asciidoctor-pdf -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/pdf/ -a pdf-theme=theme.yml -a pdf-fontsdir=".;GEM_FONTS_DIR" -o CV.pdf CV.adoc
-asciidoctor-pdf -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/pdf/ -a pdf-theme=theme.yml -a pdf-fontsdir="${CURRENT_PATH}/fonts" -o CV.pdf CV.adoc
+asciidoctor-pdf -B ${CURRENT_PATH}/ -D ${CURRENT_PATH}/outputs/pdf/ -a pdf-theme=theme.yml -a pdf-fontsdir="${CURRENT_PATH}/fonts;GEM_FONTS_DIR" -o CV.pdf CV.adoc
